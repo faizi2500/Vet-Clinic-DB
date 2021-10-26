dfe911276fd99793c98f8f9d8d35b019d8df06f7
faizi2500 <faizan.zahid2500@gmail.com>
2021-10-27 02:28:26 +0500

    Insert sql command used to create table - animals

---
+++ schema.sql
@@ -0,0 +1,9 @@
+CREATE TABLE animals(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR(65) NOT NULL,
+  date_of_birth DATE,
+  escape_attempts INT,
+  neutered BOOLEAN,
+  weight_kg DECIMAL,
+  PRIMARY KEY(id)
+);
