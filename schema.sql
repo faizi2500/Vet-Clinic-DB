71dde5df708b2b91c76cd4b080a103943ce256c5
faizi2500 <faizan.zahid2500@gmail.com>
2021-10-28 16:17:25 +0500

    Insert owners and species table and modify animals table for foreign keys

--- schema.sql
+++ schema.sql
@@ -9,3 +9,28 @@ CREATE TABLE animals(
 );
 
 ALTER TABLE animals ADD species VARCHAR(50);
+
+
+CREATE TABLE owners(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  full_name VARCHAR(65),
+  age INT,
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE species(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR(65),
+  PRIMARY KEY(id)
+);
+
+ALTER TABLE animals DROP COLUMN species;
+
+BEGIN;
+ALTER TABLE animals ADD species_id INT;
+ALTER TABLE animals ADD CONSTRAINT ft FOREIGN KEY (species_id) REFERENCES species(id);
+COMMIT;
+BEGIN;
+ALTER TABLE animals ADD owners_id INT;
+ALTER TABLE animals ADD CONSTRAINT owner_fk FOREIGN KEY (owners_id) REFERENCES owners(id);
+COMMIT;
