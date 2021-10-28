b82e5e03191d4a04873452a679ab7375df2c392e
faizi2500 <faizan.zahid2500@gmail.com>
2021-10-28 16:18:18 +0500

    Insert owners and species data and update values in animals table for added foreign keys columns

--- data.sql
+++ data.sql
@@ -10,3 +10,20 @@ INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) 
 INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES ('Devimon', '2017-05-12', 11, true, 5);
 
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Charmander', '2020-02-08', 0, false, -11), ('Plantmon', '2022-11-15', 2, true, -5.7), ('Squirtle', '1993-04-02', 3, false, -12.13), ('Angemon', '2005-06-12', 1, true, -45), ('Boarmon', '2005-06-07', 7, true, 20.4), ('Blossom', '1998-10-13', 3, false, 17);
+
+
+INSERT INTO owners (full_name, age) VALUES ('San Smith', 34), ('Jennifer Orwell', 19), ('Bob', 45), ('Melody Pond', 77), ('Dean Winchester', 14), ('Jodie Whittaker', 38);
+
+INSERT INTO species (name) VALUES ('Pokemon'), ('Digimon');
+
+BEGIN;
+UPDATE animals SET species_id = 2 WHERE name LIKE ('%mon');
+UPDATE animals SET species_id = 1 WHERE species_id IS NULL;
+COMMIT;
+
+UPDATE animals SET owners_id = 1 WHERE name = 'Agunmon';
+UPDATE animals SET owners_id = 2 WHERE name IN ('Gabumon', 'Pikachu);
+UPDATE animals SET owners_id = 2 WHERE name = 'Pikachu';
+UPDATE animals SET owners_id = 3 WHERE name IN ('Devimon', 'Plantmon');
+UPDATE animals SET owners_id = 3 WHERE id IN (5, 7, 10);
+UPDATE animals SET owners_id = 3 WHERE id IN (8, 9);
