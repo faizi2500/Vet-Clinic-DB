3842d272916e52c098aec398709d65995272a500
faizi2500 <faizan.zahid2500@gmail.com>
2021-10-29 05:46:40 +0500

    Create tables for vets, specialization and visits to the vets

--- schema.sql
+++ schema.sql
@@ -34,3 +34,26 @@ BEGIN;
 ALTER TABLE animals ADD owners_id INT;
 ALTER TABLE animals ADD CONSTRAINT owner_fk FOREIGN KEY (owners_id) REFERENCES owners(id);
 COMMIT;
+
+CREATE TABLE vets(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR(65),
+  age INT,
+  date_of_graduation DATE,
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE specialization(
+  species_id INT NOT NULL,
+  vets_id INT NOT NULL,
+  CONSTRAINT species_fk FOREIGN KEY (species_id) REFERENCES species(id),
+  CONSTRAINT vets_id FOREIGN KEY (vets_id) REFERENCES vets(id)
+);
+
+CREATE TABLE visits(
+  vets_id INT NOT NULL,
+  animals_id INT NOT NULL,
+  date_of_visit DATE NOT NULL,
+  CONSTRAINT animals_fk2 FOREIGN KEY (animals_id) REFERENCES animals(id),
+  CONSTRAINT vets_fk2 FOREIGN KEY (vets_id) REFERENCES vets(id)
+);
